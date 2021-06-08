5ffa3e4b1cfcc8f724521465cb29973294aecda6
stas137 <stasccb@gmail.com>
2021-06-09 00:27:30 +0300

    Больше деталей

--- js/utils/get-random-positive-integer.js
+++ js/utils/get-random-positive-integer.js
@@ -25,6 +25,6 @@ function getRandomPositiveInteger (min, max) {
   // И в конце с помощью метода Math.floor мы округляем полученный результат,
   // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
-};
+}
 
 getRandomPositiveInteger(1, 3);
