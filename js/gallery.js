0a1e704a155c1358d37f51342bb7c84bee888b03
stas137 <stasccb@gmail.com>
2021-06-30 12:54:00 +0300

    Отрисуй меня полностью (часть 2)

--- js/gallery.js
+++
@@ -1,7 +0,0 @@
-/* import './thumbnails-show.js';
-import {pictureShowFull} from './picture-show-full.js';
-
-const bodyElement = document.body;
-const picturesList = bodyElement.querySelector('.pictures');
-
-picturesList.addEventListener('click', (evt) => { pictureShowFull(evt.target); }); */
